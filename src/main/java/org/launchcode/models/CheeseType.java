d4fc1d978c21737ec05a1ee1f7e74e0089efb57c
John Malley <jpmalley@me.com>
2018-06-07 15:13:40 -0500

    "finished class 9 prep"

---
+++ src/main/java/org/launchcode/models/CheeseType.java
@@ -0,0 +1,18 @@
+package org.launchcode.models;
+
+public enum CheeseType {
+
+    HARD ("Hard"),
+    SOFT ("Soft"),
+    FAKE ("Fake");
+
+    private final String name;
+
+    CheeseType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
